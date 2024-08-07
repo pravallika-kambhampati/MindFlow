e7f75f15f1288c6340b32166ada05d010b247513
pravallika-kambhampati <pravallika13kambhampati@gmail.com>
2024-08-07 00:06:32 -0500

    set up tailwind

--- client/mind-flow/src/App.jsx
+++ client/mind-flow/src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/signup" exact element={<Signup />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <div>{routes}</div>;
 };
 
 export default App;
--- client/mind-flow/src/pages/Login/Login.jsx
+++ client/mind-flow/src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default Login;
