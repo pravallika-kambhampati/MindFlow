f568620abfc8128c890e89df0a0a6324130f5a49
pravallika-kambhampati <pravallika13kambhampati@gmail.com>
2024-09-21 22:57:05 -0500

    mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,10 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -13,6 +20,8 @@ app.use(
 app.get("/",(req, res) => {
     res.json({data: "hello"});
 });
+
+
 
 app.listen(8000);
 
