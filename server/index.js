6c4f798987f4debd918b272dd96b38e8227c6114
pravallika-kambhampati <pravallika13kambhampati@gmail.com>
2024-09-30 02:02:59 -0500

    note pinned api

--- server/index.js
+++ server/index.js
@@ -248,6 +248,44 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
 
 });
 
+// Update IsPinned Value
+
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+     
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+
+    if (!title && !content && !tags){
+        return res.status(400).json({error: true, message: "No changes provided"});
+    }
+    
+    try {
+        const note = await Note.findOne({_id:noteId, userId: user._id});
+
+        if(!note){
+            return res.status(404).json({error: true, message:"Note not found"});
+        }
+
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+        return res.json({
+            error: false,
+            note,
+            message: "Note updated successfully",
+        });
+    } catch (error){
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error",
+        });
+    }
+
+   
+})
+
+
 app.listen(8000);
 
 module.exports = app;
